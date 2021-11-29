7edb51995c70af2082bf51d7949aa467bc31b981
Joanes Araújo <joanescaraujo@gmail.com>
2021-11-29 20:50:56 -0300

    Exercício 4 - Ordenar uma lista numérica

---
+++ exercicio4.py
@@ -0,0 +1,12 @@
+# -*- coding: latin-1 -*-
+# -- LISTA DE EXERCÍCIOS --
+
+
+# Escreva um programa que ordene uma lista numérica com três elementos.
+
+lista = [98,26,19]  
+
+lista_ordenada = sorted(lista)
+
+print(lista)
+print(lista_ordenada)
